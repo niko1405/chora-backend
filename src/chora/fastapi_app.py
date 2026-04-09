6a3f961948cd2e016d9552b4ed1437e00d4dbded
Nikolas Vix <vini1011@h-ka.de>
2026-04-09 21:46:14 +0200

    Routers mit main router angebunden

--- src/chora/fastapi_app.py
+++ src/chora/fastapi_app.py
@@ -5,8 +5,16 @@ This module provides a simple REST API for songwriting workspace management.
 
 from fastapi import FastAPI
 
+from chora.router.artist_write_router import artist_write_router
+from chora.router.song_router import song_router
+from chora.router.song_write_router import song_write_router
+
 # Instanz der App erstellen
 app = FastAPI(title="Songs API", description="Songwriting Workspace", version="1.0.0")
+
+app.include_router(artist_write_router, prefix="/artists")
+app.include_router(song_router, prefix="/artists")
+app.include_router(song_write_router, prefix="/artists")
 
 
 @app.get("/")
