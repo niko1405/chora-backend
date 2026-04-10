c69e302246980ac1f9bddcafbb9aab461dbf5b99
Ryadh Haji <ryadh.z.haji@gmail.com>
2026-04-10 21:45:03 +0200

    __init__ für graphql

--- src/chora/graphql_api/__init__.py
+++ src/chora/graphql_api/__init__.py
@@ -1,1 +1,18 @@
 """Modul für die GraphQL-Schnittstelle."""
+from chora.graphql_api.graphql_types import (
+    ArtistInput,
+    CreatePayload,
+    Suchparameter,
+    VertragInput,
+)
+from chora.graphql_api.schema import Mutation, Query, graphql_router
+
+__all__ = [
+    "ArtistInput",
+    "CreatePayload",
+    "Mutation",
+    "Query",
+    "Suchparameter",
+    "VertragInput",
+    "graphql_router",
+]
---
+++ src/chora/graphql_api/graphql_types.py
@@ -0,0 +1,2 @@
+"""Schema für GraphQL."""
+
