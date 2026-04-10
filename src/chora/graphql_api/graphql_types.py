5afe4503ca590b5b1b1c023f6008ef3d283affd5
Ryadh Haji <ryadh.z.haji@gmail.com>
2026-04-10 21:52:18 +0200

    Inputwerte für Suchparameter

--- src/chora/graphql_api/graphql_types.py
+++ src/chora/graphql_api/graphql_types.py
@@ -1,2 +1,23 @@
 """Schema für GraphQL."""
+import email_validator
+from chora.graphql_api import VertragInput
+from gevent.ares import __all__
+
+
+@strawberry.input
+class Suchparameter:
+    """Suchparameter für Artisten."""
+
+    name: str | None = None
+    """Name des Artisten."""
+
+    genre: str | None = None
+    """Genre des Artisten."""
+
+    alter: int | None = None
+    """Alter des Artisten."""
+
+    email: str | None = None
+    """E-Mail-Adresse des Artisten."""
+
 
