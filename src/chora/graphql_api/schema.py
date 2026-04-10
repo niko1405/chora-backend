074bbf00792beb728ea3dbcab698f8019d558dfa
Ryadh Haji <ryadh.z.haji@gmail.com>
2026-04-10 21:14:39 +0200

    Query zum lesen mit Graphql

--- src/chora/graphql_api/schema.py
+++ src/chora/graphql_api/schema.py
@@ -1,5 +1,4 @@
 """Schema für GraphQL durch Strawberry."""
-from gevent.ares import __all__
 
 from typing import Final
 
@@ -10,12 +9,11 @@ from strawberry.types import Info
 
 from chora.repository.artist_repository import ArtistRepository
 from chora.repository.song_repository import SongRepository
-from chora.security.dependencies import _token_service
-from chora.security.token_service import TokenService
+from chora.security import Role, TokenService, UserService
 from chora.service import ArtistDTO
 from chora.service.artist_service import ArtistService
 from chora.service.artist_write_service import ArtistWriteService
-from chora.service.user_service import UserService
+from chora.service.exceptions import NotFoundError
 
 __all__ = ["graphql_router"]
 
@@ -27,3 +25,32 @@ _write_service: ArtistWriteService = ArtistWriteService(
     repo=_repo, song_repo=_song_repo, user_service=_user_service
 )
 _token_service: Final = TokenService()
+
+
+@strawberry.type
+class Query:
+    """GraphQL Query, um Artistdaten zu lesen."""
+
+    @strawberry.field
+    def artist(self, artist_id: strawberry.ID, info: Info) -> ArtistDTO | None:
+        """Gibt einen Artist zurück.
+
+        :param artist_id: ID des Artists
+        :param info: GraphQL Info Objekt
+        :return: Artist Objekt
+        :rtype ArtistDTO | None
+        :raises NotFoundError: Wenn kein Artist mit der angegebenen ID gefunden wird
+        """
+        logger.debug("artist_id={}", artist_id)
+
+        request: Final[Request] = info.context.get("request")
+        user: Final = _token_service.get_user_from_request(request=request)
+        if user is None:
+            return None
+
+        try:
+            artist_dto: Final = _service.find_by_id(artist_id=int(artist_id), user=user)
+        except NotFoundError:
+            return None
+        logger.debug("artist_dto={}", artist_dto)
+        return artist_dto
